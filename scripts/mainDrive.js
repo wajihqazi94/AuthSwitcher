4a47aabdf7c0d22a1c4d394f620dafe55de223bc
wajihqazi94 <49033090+wajihqazi94@users.noreply.github.com>
2019-08-21 10:44:29 -0400

    Update mainDrive.js

--- scripts/mainDrive.js
+++ scripts/mainDrive.js
@@ -71,78 +71,72 @@ geotab.addin.authoritySwitcher = function(api, state) {
 			});
 		},
 
-        populateSelect = async function(activeUser) {
+        populateSelect = function(activeUser) {
 			let currentUser = activeUser,
 				accessList = [],
-				groups = [],
-				addInData = await grabAddInData();
-			for (let groupIndex = 0; groupIndex < currentUser.companyGroups.length; groupIndex++) {
-				groups.push(currentUser.companyGroups[groupIndex].id);
-			}
-			
-			if (currentUser.companyGroups[0].id === "GroupCompanyId") {
-				for (let authorityIndex = 0; authorityIndex < addInData.authorities.length; authorityIndex++) {
-						storedData.push(addInData.authorities[authorityIndex]);
-				}
-			} else {
-				while (groups.length > 0) {
-					let currentGroup = groups.shift();
-					accessList.push(currentGroup);
-					let groupDetails = await grabGroup(currentGroup),
-						children = groupDetails[0].children;
-					if (children.length > 0) {
-						for (let childIndex = 0; childIndex < children.length; childIndex++) {
-							groups.push(children[childIndex].id);
-						}
-					}
+				groups = [];
+				
+			grabAddInData().then(function(addInData) {
+					for (let groupIndex = 0; groupIndex < currentUser.companyGroups.length; groupIndex++) {
+					groups.push(currentUser.companyGroups[groupIndex].id);
 				}
 				
-				for (let authorityIndex = 0; authorityIndex < addInData.authorities.length; authorityIndex++) {
-					if (contain(addInData.authorities[authorityIndex].groups, accessList)) {
-						storedData.push(addInData.authorities[authorityIndex]);
+				if (currentUser.companyGroups[0].id === "GroupCompanyId") {
+					for (let authorityIndex = 0; authorityIndex < addInData.authorities.length; authorityIndex++) {
+							storedData.push(addInData.authorities[authorityIndex]);
+					}
+				} else {
+					while (groups.length > 0) {
+						let currentGroup = groups.shift();
+						accessList.push(currentGroup);
+						grabGroup(currentGroup).then(function(groupDetails) {
+							let children = groupDetails[0].children;
+							if (children.length > 0) {
+								for (let childIndex = 0; childIndex < children.length; childIndex++) {
+									groups.push(children[childIndex].id);
+								}
+							}
+						})
+					}
+					
+					for (let authorityIndex = 0; authorityIndex < addInData.authorities.length; authorityIndex++) {
+						if (contain(addInData.authorities[authorityIndex].groups, accessList)) {
+							storedData.push(addInData.authorities[authorityIndex]);
+						}
+					}
+				};
+				
+				if (storedData.length > 0) {
+
+					//Grab All Data and display
+					for (let i = 0; i < storedData.length; i++) {
+						let authSelect = document.getElementById("importauthorities");
+
+						storedData.sort(function(a, b) {
+							if (a.authorityName.toLowerCase() < b.authorityName.toLowerCase()) {
+								return -1;
+							}
+							if (a.authorityName.toLowerCase() > b.authorityName.toLowerCase()) {
+								return 1;
+							}
+							return 0;
+						});
+						for (i = 0; i < storedData.length; i++) {
+							if (storedData[i].authorityName !== activeUser.authorityName) {
+								let authOption = new Option();
+								authOption.text = storedData[i].authorityName;
+								authOption.value = storedData[i].authorityName;
+								authSelect.add(authOption);
+							}
+						}
 					}
 				}
-			};
+			})
 			
-			if (storedData.length > 0) {
-
-				//Grab All Data and display
-				for (let i = 0; i < storedData.length; i++) {
-					let authSelect = document.getElementById("importauthorities");
-
-					storedData.sort(function(a, b) {
-						if (a.authorityName.toLowerCase() < b.authorityName.toLowerCase()) {
-							return -1;
-						}
-						if (a.authorityName.toLowerCase() > b.authorityName.toLowerCase()) {
-							return 1;
-						}
-						return 0;
-					});
-					for (i = 0; i < storedData.length; i++) {
-						if (storedData[i].authorityName !== activeUser.authorityName) {
-							let authOption = new Option();
-							authOption.text = storedData[i].authorityName;
-							authOption.value = storedData[i].authorityName;
-							authSelect.add(authOption);
-						}
-					}
-				}
-			}
         };
 
 
     return {
-        /**
-         * initialize() is called only once when the Add-In is first loaded. Use this function to initialize the
-         * Add-In's state such as default values or make API requests (MyGeotab or external) to ensure interface
-         * is ready for the user.
-         * @param {object} api - The GeotabApi object for making calls to MyGeotab.
-         * @param {object} state - The page state object allows access to URL, page navigation and global group filter.
-         * @param {function} addInReady - Call this when your initialize route is complete. Since your initialize routine
-         *        might be doing asynchronous operations, you must call this method when the Add-In is ready
-         *        for display to the user.
-         */
         initialize: function(api, state, addInReady) {
 
             api.getSession(function(session) {
@@ -157,32 +151,11 @@ geotab.addin.authoritySwitcher = function(api, state) {
 			save.addEventListener("click", function() {
                 saveNewInfo();
             }, false);
-            // MUST call addInReady when done any setup
 			addInReady();
         },
-
-        /**
-         * focus() is called whenever the Add-In receives focus.
-         *
-         * The first time the user clicks on the Add-In menu, initialize() will be called and when completed, focus().
-         * focus() will be called again when the Add-In is revisited. Note that focus() will also be called whenever
-         * the global state of the MyGeotab application changes, for example, if the user changes the global group
-         * filter in the UI.
-         *
-         * @param {object} api - The GeotabApi object for making calls to MyGeotab.
-         * @param {object} state - The page state object allows access to URL, page navigation and global group filter.
-         */
         focus: function(api, state) {
             
         },
-        /**
-         * blur() is called whenever the user navigates away from the Add-In.
-         *
-         * Use this function to save the page state or commit changes to a data store or release memory.
-         *
-         * @param {object} api - The GeotabApi object for making calls to MyGeotab.
-         * @param {object} state - The page state object allows access to URL, page navigation and global group filter.
-         */
         blur: function(api, state) {
 
 
